e6a2ffc4c2ea2a36042dd73f985545b4013bd9a3
mikearbib <arbib.michael@gmail.com>
2016-08-25 21:05:08 +0200

    création des fichiers de classe

---
+++ Blokus/src/blokus/Blokus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blokus;
+
+/**
+ *
+ * @author Squall
+ */
+public class Blokus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Accueil a = new Accueil();
+        a.setVisible(true);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # java_blokus
+Ce projet est réalisé dans le contexte du Master 2 PISE de l'Université Paris 7 Diderot.
+
+Il a pour but la réalisation du jeu de plateau 'Blokus' en Java
